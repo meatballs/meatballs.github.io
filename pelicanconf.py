61273d3158606e01030cce6e16ccf4abe051d850
Owen Campbell <owen.campbell@empiria.co.uk>
2015-05-14 11:18:43 +0100

    gets pelican-bootstrap3 theme working

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,24 +17,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
-
-SOCIAL = (
-    ('Google+', 'http://plus.google.com/+OwenCampbell1',
-        'fa fa-google-plus-square fa-fw fa-lg'),
-    ('Twitter', 'https://twitter.com/opcampbell',
-        'fa fa-twitter-square fa-fw fa-lg'),
-    ('LinkedIn', 'https://www.linkedin.com/in/owencampbell',
-        'fa fa-linkedin-square fa-fw fa-lg'),
-    ('BitBucket', 'http://bitbucket.org/meatballs',
-        'fa fa-bitbucket-square fa-fw fa-lg'),
-    ('GitHub', 'http://github.com/meatballs',
-        'fa fa-github-square fa-fw fa-lg'),)
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
@@ -43,4 +25,11 @@ RELATIVE_URLS = True
 PLUGIN_PATHS = ('plugins',)
 PLUGINS = ['creole_reader', ]
 
-THEME = 'themes/voidy-bootstrap/'
+THEME = 'themes/pelican-bootstrap3/'
+BOOTSTRAP_THEME = 'cerulean'
+SOCIAL = (
+    ('Google+', 'http://plus.google.com/+OwenCampbell1'),
+    ('Twitter', 'https://twitter.com/opcampbell'),
+    ('LinkedIn', 'https://www.linkedin.com/in/owencampbell'),
+    ('BitBucket', 'http://bitbucket.org/meatballs'),
+    ('GitHub', 'http://github.com/meatballs'))
