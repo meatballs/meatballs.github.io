c014254ccc15ad4cdb1d3bbba2662b96d044c467
Owen Campbell <owen.campbell@empiria.co.uk>
2015-05-14 12:11:50 +0100

    removes categories from menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,20 +7,16 @@ SITENAME = u'Owen Campbell'
 SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
-
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = u'en_GB'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
+DISPLAY_CATEGORIES_ON_MENU = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
 
 PLUGIN_PATHS = ('plugins',)
 PLUGINS = ['creole_reader', ]
