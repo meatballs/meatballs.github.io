c53772f3d077fc9d79e65df372da12d1ec800132
Owen Campbell <owen.campbell@tanti.org.uk>
2018-06-12 08:41:51 +0100

    Use https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.owencampbell.me.uk'
+SITEURL = 'https://www.owencampbell.me.uk'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
